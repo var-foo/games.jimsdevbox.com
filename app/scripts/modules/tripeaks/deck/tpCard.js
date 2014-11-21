96150ce0bd52bf725e3c464f2f07c5745ae5fdea
Jim Munro <jimmunro@quickenloans.com>
2014-11-21 15:57:16 -0500

    performance enhancements

--- app/scripts/modules/tripeaks/deck/tpCard.js
+++ app/scripts/modules/tripeaks/deck/tpCard.js
@@ -1,28 +1,25 @@
-angular.module('tripeaks').directive('tpCard', function($timeout, Field, $animate){
+angular.module('tripeaks').directive('tpCard', function(Field, $animate){
     "use strict";
 
     return {
         require: '^tpField',
         scope: {
-            card: '='
+            card: '=',
+            tpCard: '='
         },
         link: function(scope,elem,attrs,tpField){
-            // Timeout is here because isPeak doesn't calculate right away, so this avoids the race condition
-            $timeout(function(){
-                tpField.registerCard(elem);
-                scope.card.id = attrs.id;
-                scope.card.isPeak = elem.hasClass('peak');
-                elem.click(function(){
-                    Field.removeFieldCard(scope.card);
-                });
+            tpField.registerCard(elem);
+            scope.card.id = attrs.id;
+            scope.card.isPeak = scope.tpCard < 3;
+            elem.click(function(){
+                Field.removeFieldCard(scope.card);
             });
 
             scope.$on('fieldCardRemoved', function(){
-                    if(tpField.askToFlip(elem, scope.card.value) && elem.hasClass('back')){
-                        $animate.removeClass(elem, 'back');
-                        $animate.addClass(elem, 'front');
-                    }
-
+                if(tpField.askToFlip(elem, scope.card.value) && elem.hasClass('back')){
+                    $animate.removeClass(elem, 'back');
+                    $animate.addClass(elem, 'front');
+                }
             });
         }
     };
